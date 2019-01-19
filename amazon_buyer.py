da75b2c72f44000b700710c7877022c03e860b9c
Chand Chen <changduchen@gmail.com>
2019-01-19 13:52:26 +0800

    Reorganize program function to separate single and multi product

--- amazon_buyer.py
+++ amazon_buyer.py
@@ -17,7 +17,7 @@ def webdriver_login(driver, account, passwd):
 
 
 def robot_check(driver):
-
+    """ Check if current page is robot verification page """
     if driver.title == 'Robot Check':
         time.sleep(1)
         try:
@@ -37,13 +37,62 @@ def robot_check(driver):
     return
 
 
-def auto_run_amazon():
+def single_product_ordering(driver):
+    """ Ordering for single product page """
+    driver.get(PRODUCTS)
+    try:
+        offers = driver.find_element_by_id(
+            'olpOfferList').find_elements_by_class_name('olpOffer')
+        for offer in offers:
+            seller = offer.find_element_by_class_name(
+                'olpSellerName').text
+            if seller not in WHITE_LIST:
+                offer.find_element_by_name('submit.addToCart').click()
+                time.sleep(1)
+                driver.find_element_by_id('hlb-ptc-btn-native').click()
+                time.sleep(1)
+                single_product_ordering(driver)
+    except Exception as e:
+        return
+
+
+def multi_type_product_ordering(driver):
+    """ Ordering for multiple type of product page """
+    driver.get(PRODUCTS)
+    try:
+        variations = driver.find_element_by_id(
+            'variationsTwister').find_elements_by_xpath('ul')
+    except Exception as e:
+        variations = []
+    for size_id, var_type in enumerate(variations):
+        try:
+            contents = var_type.find_elements_by_xpath('li')
+        except Exception as e:
+            contents = []
+        for color_id, content in enumerate(contents):
+            driver.get(DYNAMIC_PRODUCTS.format(color_id, size_id))
+
+            offers = driver.find_element_by_id(
+                'olpOfferList').find_elements_by_class_name('olpOffer')
+            for offer in offers:
+                seller = offer.find_element_by_class_name(
+                    'olpSellerName').text
+                if seller not in WHITE_LIST:
+                    offer.find_element_by_name('submit.addToCart').click()
+                    time.sleep(1)
+                    driver.find_element_by_id('hlb-ptc-btn-native').click()
+                    break
+
+
+def run_amazon_buyer():
+    """ Main function of amazon buyer program """
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--proxy-server=http://{}'.format(PROXY))
     chrome_options.add_argument("--start-maximized")
     driver = webdriver.Chrome(chrome_options=chrome_options)
 
     driver.get("http://httpbin.org/ip")
+
     if PROXY_ORIGIN in driver.page_source:
         driver.get(SITE_URL)
         robot_check(driver)
@@ -52,32 +101,14 @@ def auto_run_amazon():
         driver = webdriver_login(driver, USERNAME, PASSWORD)
         time.sleep(1)
 
-        driver.get(PRODUCTS)
-        try:
-            variations = driver.find_element_by_id(
-                'variationsTwister').find_elements_by_xpath('ul')
-        except Exception as e:
-            variations = []
-        for size_id, var_type in enumerate(variations):
-            try:
-                contents = var_type.find_elements_by_xpath('li')
-            except Exception as e:
-                contents = []
-            for color_id, content in enumerate(contents):
-                driver.get(DYNAMIC_PRODUCTS.format(color_id, size_id))
+        # driver.get(PRODUCTS)
 
-                offers = driver.find_element_by_id(
-                    'olpOfferList').find_elements_by_class_name('olpOffer')
-                for offer in offers:
-                    seller = offer.find_element_by_class_name(
-                        'olpSellerName').text
-                    if seller not in WHITE_LIST:
-                        offer.find_element_by_name('submit.addToCart').click()
-                        time.sleep(1)
-                        driver.find_element_by_id('hlb-ptc-btn-native').click()
-                        break
+        single_product_ordering(driver)
+
+        # multi_type_product_ordering(driver)
+
     driver.quit()
 
 
 if __name__ == '__main__':
-    auto_run_amazon()
+    run_amazon_buyer()
