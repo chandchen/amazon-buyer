6275489d8fe3b2f44d990ed92b72ee327778cc32
Chand Chen <changduchen@gmail.com>
2019-01-19 11:44:50 +0800

    Update configs for user login infos

--- settings.py
+++ settings.py
@@ -6,6 +6,15 @@ PASSWORD = 'password'
 PROXY = '127.0.0.1:1080'
 PROXY_ORIGIN = '127.0.0.1'
 
+LOGIN_USERS = [
+    {
+        'username': 'username',
+        'password': 'password',
+        'proxy': 'proxy',
+        'proxy_origin': 'proxy_origin'
+    },
+]
+
 PRODUCTS = 'product_link'
 DYNAMIC_PRODUCTS = 'dynamic_product_link'
 
